47e0a30ab68d1724f2990e710a5fc5baeb706de2
Alphaandsew <alfonso.martinez@shrikefound.com>
2020-09-21 11:44:42 -0700

    init commit
    create user/blog posts. add bootstrap

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # alfonso@mail.com -> 
+  def user_name
+    return email.split("@")[0].capitalize
+  end
+
+
+end
